1e3f908022ff33405ea74308f0dc72dabedd4866
samuelpg <9706510+samuelpg@users.noreply.github.com>
2019-12-10 15:34:48 +0100

    docstrings of user

--- facebook/User.py
+++ facebook/User.py
@@ -3,19 +3,98 @@ class User:
     Class for implementing User
 
     """
-    def __init__(self):
+    def __init__(self, name, birthday, location):
+        """
+        Constructor for the User class
+
+        Parameters
+        ----------
+        name: str
+            name of the user
+        birthday: str
+            birthday of the user
+        location: str
+            current location
+
+        Returns
+        -------
+        Bool
+            1 if it successfully created the user
+            0 otherwise
+
+        """
+        pass
+
+    def modifyBio(self, bio):
+        """
+        Method for modifying Bio section of profile
+
+
+        Parameters
+        ---------
+        bio: str
+            new bio for the profile
+
+        Returns
+        -------
+        Bool
+            1 if it successfully modified bio
+            0 otherwise
+        """
         pass
 
     def addFriend(self, user):
+        """
+        Method for adding friend
+
+
+        Parameters
+        ---------
+        user: User
+            User to add to friend list
+
+        Returns
+        -------
+        Bool
+            1 if it successfully added the user to friend list
+            0 otherwise
+        """
         pass
 
     def joinGroup(self, group):
+        """
+        Method for joining group
+
+
+        Parameters
+        ---------
+        group: Group
+            group to join
+
+        Returns
+        -------
+        Bool
+            1 if it successfully added the user to friend list
+            0 otherwise
+        """
         pass
     
     def createPost(self, content):
+        """
+        Method to add friend
+
+        Parameters
+        ----------
+        """
         pass
 
     def deletePost(self, post):
+        """
+        Method to add friend
+
+        Parameters
+        ----------
+        """
         pass
 
     
