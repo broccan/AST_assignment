81e28e02400a262b07809941b6f6a1bf07ae7328
Brennan <bjp209@hotmail.com>
2020-01-08 20:03:16 +0100

    User tests implemented

--- test/User_tests.py
+++ test/User_tests.py
@@ -15,6 +15,29 @@ class user_unit_tests(unittest.TestCase):
         except:
             self.fail("Unexpected exception when initialising user")
     
-    def test_user_except(self):
-        with self.assertRaises(TypeError):
-            User(1, 3, 4)
+    # def test_user_except(self):
+    #     with self.assertRaises(TypeError):
+    #         User(1, 3, 4)
+
+    def test_modify_bio(self):
+        user = User("joe", "23/09/2000", "Bonn")
+        self.assertTrue(user.modifyBio("test Bio"))
+
+    def test_addFriend(self):
+        user1 = User("joe", "23/09/2000", "Bonn")
+        user2 = User("adam", "23/09/2000", "Bonn")
+        self.assertTrue(user1.addFriend(user2))
+
+    def test_joinGroup(self):
+        user1 = User("joe", "23/09/2000", "Bonn")
+        user2 = User("adam", "23/09/2000", "Bonn")
+        group = Group("Food Lovers", user2)
+        self.assertTrue(user1.joinGroup(group))
+
+    def test_createPost(self):
+        user = User("joe", "23/09/2000", "Bonn")
+        self.assertTrue(user.createPost("test post"))
+
+if __name__ == "__main__":
+    unittest.main()
+
