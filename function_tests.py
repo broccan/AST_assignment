5f9c94b3c14c66b78e818f42326c8f41e4bf5c37
Brennan <bjp209@hotmail.com>
2020-01-08 15:12:27 +0100

    Added testcase skeleton for people to fill in

--- function_tests.py
+++ function_tests.py
@@ -1,1 +1,12 @@
+import unittest
 
+
+class user_unit_tests(unittest.TestCase):
+
+class post_unit_tests(unittest.TestCase):
+
+class group_unit_tests(unittest.TestCase):
+
+class timeline_unit_tests(unittest.TestCase):
+
+    
