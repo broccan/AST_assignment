d0275d02877703f29f107da0b19d84f570d8ccf9
Brennan <bjp209@hotmail.com>
2020-01-08 19:17:53 +0100

    Fixed import problems

--- test/Timeline_tests.py
+++ test/Timeline_tests.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+sys.path.append(".")
+sys.path.append('..')
 
 
 class timeline_unit_tests(unittest.TestCase):
--- test/User_tests.py
+++ test/User_tests.py
@@ -1,4 +1,20 @@
 import unittest
+import sys
+sys.path.append(".")
+sys.path.append('..')
+from facebook.User import User
+from facebook.Group import Group
+from facebook.Timeline import Timeline
+from facebook.Post import Post
 
 
 class user_unit_tests(unittest.TestCase):
+    def test_user_init(self):
+        try:
+            User("joe", "23/09/2000", "Bonn")
+        except:
+            self.fail("Unexpected exception when initialising user")
+    
+    def test_user_except(self):
+        with self.assertRaises(TypeError):
+            User(1, 3, 4)
