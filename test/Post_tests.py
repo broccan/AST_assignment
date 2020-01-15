d2ba385e516dde836be37366bac412ba3152c8c4
AshAswin <aswinkumar.work@gmail.com>
2020-01-15 11:42:07 +0100

    Add Unit tests for Post and Timeline classes

--- test/Post_tests.py
+++ test/Post_tests.py
@@ -1,8 +1,12 @@
 import unittest
 import sys
+from facebook.Post import Post
 sys.path.append(".")
 sys.path.append('..')
 
 
 class PostUnitTests(unittest.TestCase):
-    pass
+    def test_addLike(self):
+        post = Post("test_user","test post")
+        post.addLike("liking_user")
+        self.assertEqual(post.likes[0],"liking_user")
--- test/Timeline_tests.py
+++ test/Timeline_tests.py
@@ -1,8 +1,12 @@
 import unittest
 import sys
+from facebook.Timeline import Timeline
 sys.path.append(".")
 sys.path.append('..')
 
 
 class TimelineUnitTests(unittest.TestCase):
-    pass
+    def test_addPost(self):
+        timeline = Timeline()
+        timeline.addPost("test post")
+        self.assertEqual(timeline.posts[0],"test post")
